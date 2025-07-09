1d8cb45a22aa3d45f49be1fd2fc2cf915a5de77c
Kunal-Vala <kunalvala2005@gmail.com>
2025-07-09 09:17:35 +0530

    icons issue fix

--- src/ui.js
+++ src/ui.js
@@ -1,6 +1,6 @@
 function getIconPath(iconName) {
   // You may need to adjust the path based on your build setup
-  return `../assets/icons/${iconName}.svg`;
+  return `./assets/icons/${iconName}.svg`;
 }
 
 export { getIconPath };
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -8,11 +9,17 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    // publicPath: '/', // Use '/' if testing locally or using relative links
     clean: true,
   },
   devtool: 'eval-source-map',
   devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
     watchFiles: ['./src/template.html'],
+    open: true,
+    hot: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -21,6 +28,9 @@ module.exports = {
     new ESLintPlugin({
       extensions: ['js', 'mjs', 'cjs'],
       fix: true,
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'src/assets/icons', to: 'assets/icons' }],
     }),
   ],
   module: {
